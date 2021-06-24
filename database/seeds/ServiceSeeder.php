626897419ad64c1ebbf0ef76877966f90bf5e2ca
wvwall <waltervelardo@gmail.com>
2021-06-24 15:22:42 +0200

    seeder_service

---
+++ database/seeds/ServiceSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Service;
+use Illuminate\Support\Str;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $services = ['wifi','car','pool','concierge','sauna'];
+        
+        for ($i=0; $i < count($services) ; $i++) { 
+            $new_service = new Service();
+            $new_service->name = $services[$i];
+            $new_service->save();
+        }
+    }
+}
