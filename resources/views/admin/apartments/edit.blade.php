3be7ecb88b812ea5d77db5dc7cc70bf34691d7f1
wvwall <waltervelardo@gmail.com>
2021-06-30 16:00:48 +0200

    css_3

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-      <div class="col-md-12 justify-content-center" style="display: flex">
+      <div id="edit" class="col-md-12 justify-content-center" style="display: flex">
         <h4>Modifica appartamento</h4>
       </div>
       <div class="col-md-8 justify-content-center">
@@ -17,12 +17,12 @@
           @endif
       </div>
       <div class="col-md-8">
-      <img src="{{ asset('storage/' . $apartment->thumb) }}" class="card-img-top" style="width: 250px;" alt="immagine non disponibile">
+      <img src="{{ asset('storage/' . $apartment->thumb) }}" class="card-img-top" style="width: 250px;" style="border-radius: 10px;" alt="immagine non disponibile">
         <form class="crea" action="{{route('admin.apartments.update', ['apartment' => $apartment->id])}}" method="post" enctype="multipart/form-data" @click="dati">
           @csrf
           @method('PATCH')
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Title</label>
+            <label for="exampleFormControlInput1" class="form-label">Titolo</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"  placeholder="{{ old('title', $apartment->title) }}" value="{{ old('title', $apartment->title) }}">
             @error('title')
               <small class="text-danger">{{ $message }}</small>
@@ -30,7 +30,7 @@
           </div>
 
           <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">City</label>
+            <label for="exampleFormControlTextarea1" class="form-label">Città</label>
             <input type="text" class="form-control @error('city') is-invalid @enderror" placeholder="{{ old('city', $apartment->city) }}" value="{{ old('city', $apartment->city) }}" name="city" v-model="ins_citta"></input>
             @error('city')
               <small class="text-danger">{{ $message }}</small>
@@ -38,14 +38,14 @@
           </div>
 
           <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Address</label>
+            <label for="exampleFormControlTextarea1" class="form-label">Indirizzo</label>
             <input type="text" class="form-control @error('address') is-invalid @enderror" name="address"  value="{{ old('city', $apartment->address) }}" v-model="ins_indirizzo"></input>
             @error('address')
               <small class="text-danger">{{ $message }}</small>
             @enderror
           </div>
 
-          <button class="btn btn-primary" type="button" name="button" @click="addressSugg(ins_indirizzo, ins_citta)">validate address</button>
+          <button class="btn btn-primary" type="button" name="button" @click="addressSugg(ins_indirizzo, ins_citta)">Valida indirizzo</button>
           <ul>
             <li class="form-control" @click="saveAddress(addressEl)" class="address-suggestion" v-for="(addressEl, i) in validAddresses" v-if="validAddresses!=[]">
                @{{ addressEl.address.freeformAddress }} - @{{ addressEl.address.streetName }} - @{{ addressEl.address.municipality }}
@@ -59,21 +59,21 @@
 
 
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Rooms</label>
+            <label for="exampleFormControlInput1" class="form-label">Stanze</label>
             <input type="number" class="form-control-file @error('n_rooms') is-invalid @enderror" id="n_rooms" name="n_rooms" placeholder="{{ old('n_rooms', $apartment->n_rooms) }}" value="{{ old('n_rooms', $apartment->n_rooms) }}">
             @error('n_rooms')
               <small class="text-danger">{{ $message }}</small>
             @enderror
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Beds</label>
+            <label for="exampleFormControlInput1" class="form-label">Letti</label>
             <input type="number" class="form-control-file @error('n_beds') is-invalid @enderror" id="n_beds" name="n_beds" placeholder="{{ old('n_beds', $apartment->n_beds) }}" value="{{ old('n_beds', $apartment->n_beds) }}">
             @error('n_beds')
               <small class="text-danger">{{ $message }}</small>
             @enderror
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">n_bathrooms</label>
+            <label for="exampleFormControlInput1" class="form-label">Bagni</label>
             <input type="number" class="form-control-file @error('n_bathrooms') is-invalid @enderror" id="n_bathrooms" name="n_bathrooms" placeholder="{{ old('n_bathrooms', $apartment->n_bathrooms) }}" value="{{ old('n_bathrooms', $apartment->n_bathrooms) }}">
             @error('n_bathrooms')
               <small class="text-danger">{{ $message }}</small>
@@ -88,7 +88,7 @@
             </div>
           </div> -->
           <div class="mb-3" @click="dati">
-            <label for="exampleFormControlInput1" class="form-label">Square Meters</label>
+            <label for="exampleFormControlInput1" class="form-label">Metri quadrati</label>
             <input type="number" class="form-control-file @error('square_meters') is-invalid @enderror" id="square_meters" name="square_meters" placeholder="{{ old('square_meters', $apartment->square_meters) }}" value="{{ old('square_meters', $apartment->square_meters) }}">
             <span>m2</span>
             @error('square_meters')
@@ -102,9 +102,9 @@
           </div>
           @endforeach
           <div class="mb-3">
-            <label for="thumb" class="form-label">Thumb</label>
+            <label for="thumb" class="form-label">Foto</label>
             <input type="file" class="form-control-file @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="">
-            <span>Upload Main Image</span>
+            <span>Carica foto principale</span>
             @error('thumb')
               <small class="text-danger">{{ $message }}</small>
             @enderror
@@ -119,7 +119,7 @@
             </div>
           </div>
 
-          <button type="submit" name="button">Save</button>
+          <button class="btn btn-primary" type="submit" name="button">Aggiungi</button>
         </form>
       </div>
     </div>
